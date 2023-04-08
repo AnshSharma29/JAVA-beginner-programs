45a951f90f0108e035a0950295c35ea55762d442
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-04-08 21:11:46 +0530

    Add files via upload

---
+++ Program 2.java
@@ -0,0 +1,41 @@
+#WAP to check whether a given number exists in the array or not.
+
+import java.util.*;
+
+public class Program2{
+
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		int arr[]=new int[10];
+
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print("Enter array elements: ");
+			arr[i]=sc.nextInt();
+		}
+
+		System.out.print("Enter the value to be found: ");
+		int number=sc.nextInt();
+		int temp=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			if(int number == arr[i])
+			{
+				System.out.print("Number found. ");
+				temp=1;
+				break;
+			}
+
+		}
+		
+		if(temp==0)
+		{
+			System.out.print("Number not found.");
+
+		}
+
+
+	}
+
+}
