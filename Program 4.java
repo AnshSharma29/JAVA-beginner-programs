c2d4c1464596f13dfd859d9c97ea8a2b807bf175
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-04-12 00:33:27 +0530

    Add files via upload

---
+++ Program 4.java
@@ -0,0 +1,33 @@
+#WAP to create a class circle & calculate its area and circumference.
+
+import java.util.*;
+
+class Circle{
+	public double r;
+
+	public double circumference(){
+
+		return 2*3.14*r;
+	}
+
+	public double area(){
+
+	return 3.14*r*r;
+	}
+
+class Mymain{
+
+	public static void main(String args[]){
+
+		Circle aCircle = new Circle();
+		aCircle.r =5;
+		double area = aCircle.area();
+		double circum = aCircle.circumference();
+		System.out.println("Area = "+area);
+		System.out.println("Circumference = "+circum);
+	}
+
+}
+
+
+
