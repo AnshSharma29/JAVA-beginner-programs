0a5449a34106989087615eda41f9adc5ca379e90
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-04-21 23:30:00 +0530

    Update Program6.java

--- Program6.java
+++ Program6.java
@@ -1,14 +1,41 @@
+// WAP to demonstrate Method overloading.
+
+import java.util.*;
+
+class OverloadDemo{
+	void test(){
+		
+		System.out.println("No parameters.");
+	}
+	
+	void test(int a){
+
+		System.out.println("a : "+a);
+	}
+
+	void test(int a, int b){
+
+		System.out.println("a and b: "+a+" "+b);
+	}
+
+	void test(double a){
+
+		System.out.println("double a: "+a);
+	}
+
+}
+
+class Program6{
+
+	public static void main(String args[])
+	{
+
+		OverloadDemo ob = new OverloadDemo();
+		double result;
+		ob test;
+		ob test1;
+	}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+}
