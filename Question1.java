a18a09fc5db8c1435bd0036538036d7111780801
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-05-07 00:09:07 +0530

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,47 +1,24 @@
-//WAP to show
+//WAP to find out factorial of a number through recursion.
 
+package question1;
+import java.util.*;
+public class Question1 {
+    
+    static int factorial(int a){
+        if(a==0)
+            return 1;
+        else{
+            return (a*factorial(a-1));
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public static void main(String[] args) {
+        
+        int i,fact=1;
+        int number=6;
+        fact=factorial(number);
+        System.out.println("Factorial of "+number+" is: "+fact);
+        
+    }
+}
 
