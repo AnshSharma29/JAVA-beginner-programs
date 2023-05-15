876399dfd9be32590af00e6b2c94cf7b7f508320
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-05-16 00:27:38 +0530

    Create Program7.java

---
+++ Program7.java
@@ -0,0 +1,19 @@
+//Creating a thread using runnable interface.
+import java.util.*;
+class Mythread implements Runnable
+{
+  public void run()
+  {
+    System.out.println("This thread is running.");
+  }
+}
+
+class Program7
+{
+  public static void main(String args[])
+  {
+    Mythread ob = new Mythread();
+    Thread t = new Thread(ob);
+    t.start();
+  }
+}
