4eb7e23009b5eb52a81add2e52e621effec886ab
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-05-28 15:15:16 +0530

    Create Program9.java

---
+++ Program9.java
@@ -0,0 +1,24 @@
+// WAP to accept a 3 x 3 matrix from the user and display the left diagonal elements only
+
+import java.util.Scanner;
+public class Program9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[3][3];
+        System.out.println("Enter the elements of the matrix: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("The left diagonal elements are: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i == j) {
+                    System.out.println(arr[i][j] + " ");
+                }
+            }
+        }
+        sc.close();
+    }
+}
