8d09799f6a66c6eab9fa5d621f525444fbb55356
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-05-28 15:13:55 +0530

    Create Program8.java
    This code was completed with the help of Kanishk Rawat

---
+++ Program8.java
@@ -0,0 +1,25 @@
+// WAP to create 2 x 2 matrix and display the sum. Accept the matrix from the user.
+
+import java.util.Scanner;
+
+public class Program8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[2][2];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print("Enter array elements: ");
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                sum = sum + arr[i][j];
+            }
+        }
+        System.out.println("Sum of the matrix is: " + sum);
+        sc.close();
+    }
+}
+
